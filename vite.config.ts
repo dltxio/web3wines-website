c8cbffdad1d81b88d679d65810492dd55e6a659b
Brett van Niekerk <79682833+brettvniekerk@users.noreply.github.com>
2023-02-02 16:33:01 +1000

    feat: favicon fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,12 @@
 import { defineConfig, loadEnv, UserConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { ViteFaviconsPlugin } from "vite-plugin-favicon";
 
 // https://vitejs.dev/config/
 export default ({ mode }: UserConfig) => {
   process.env = { ...process.env, ...loadEnv(mode!, process.cwd()) };
 
   return defineConfig({
-    plugins: [
-      react(),
-      ViteFaviconsPlugin({
-        logo: "./src/images/WineGlass.png",
-        favicons: {
-          appName: "Web3 Wines",
-          appDescription: "The official website for Web3 Wines",
-          developerName: "Brett van Niekerk",
-          developerURL: "https://github.com/brettvniekerk"
-        }
-      })
-    ],
+    plugins: [react()],
     define: {
       "process.env": process.env
     }
