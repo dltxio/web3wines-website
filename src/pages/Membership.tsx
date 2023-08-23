9bfbe880103fca23b0094d71969490f3e39250f1
maiha <maiha.219@gmail.com>
2023-08-23 16:07:18 +1000

    working on Membership page

--- src/pages/Membership.tsx
+++ src/pages/Membership.tsx
@@ -11,7 +11,35 @@ const Membership: React.FC = () => {
 
     return (
         <PageLayout>
+            <div className="overflow-hidden bg-white py-24 sm:py-32">
+                <h2 className="text-4xl font-bold tracking-tight text-black text-center sm:text-6xl">Membership</h2>
+                <div className="mx-auto max-w-7xl px-6 lg:px-8">
+                    <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+                        <div className="lg:pr-8 lg:pt-4">
+                            <div className="lg:max-w-lg">
+                                <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Benefits of becoming our member</p>
+                                <ul className="mt-6 text-lg leading-8 text-gray-600">
+                                    <li>Enhanced Transparency</li>
+                                    <li>Anti-Counterfeiting</li>
+                                    <li>Quality Assurance</li>
+                                    <li>Consumer Engagement</li>
+                                    <li>Regulatory Compliance</li>
+                                    <li>Efficient Supply Chain</li>
+                                    <li>Data Security</li>
+                                </ul>
 
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Joining process</p>
+                        <p className="mt-6 text-lg leading-8 text-gray-600">
+                            Give us an understanding of your appetite around getting involved in the Web3 Wines and implementing blockchain
+                            technology in your next vintage. Email us at info@web3wines.org. Our team look forward to working with you!
+                        </p>
+                    </div>
+                </div>
+            </div>
         </PageLayout>
     );
 };
