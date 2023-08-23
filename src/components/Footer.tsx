179b2f64a803c97da1a2eec5bb494679b594ecdd
maiha <maiha.219@gmail.com>
2023-08-23 11:36:26 +1000

    working on Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,43 @@
+import { Typography } from "@material-tailwind/react";
+
 export function Footer() {
     return (
-        <footer className="relative w-full mx-auto flex flex-col md:flex-row justify-center items-center bg-navbar p-4">
-            <div className="container">
-                <h1 className="text-3xl font-semibold">Find Us</h1>
-                <ul>
-                    <li>Address: Level 18, Waterfront Place, 1 Eagle Street, Brisbane QLD 4000, Australia</li>
-                    <li>Email: info@web3wines.org</li>
-                </ul>
-            </div>
+        <footer className="flex w-full flex-row flex-wrap items-center justify-center gap-y-6 gap-x-12 border-t border-blue-gray-50 py-6 text-center md:justify-between bg-navbar">
+            <Typography color="blue-gray" className="font-normal">
+                &copy; 2023 Web3 Wines. All rights reserved.
+            </Typography>
+            <ul className="flex flex-wrap items-center gap-y-2 gap-x-8">
+                <li>
+                    <Typography
+                        as="a"
+                        href="/about_us"
+                        color="blue-gray"
+                        className="font-normal transition-colors hover:text-blue-500 focus:text-blue-500"
+                    >
+                        About Us
+                    </Typography>
+                </li>
+                <li>
+                    <Typography
+                        as="a"
+                        href="/faqs"
+                        color="blue-gray"
+                        className="font-normal transition-colors hover:text-blue-500 focus:text-blue-500"
+                    >
+                        FAQs
+                    </Typography>
+                </li>
+                <li>
+                    <Typography
+                        as="a"
+                        href="/contact_us"
+                        color="blue-gray"
+                        className="font-normal transition-colors hover:text-blue-500 focus:text-blue-500"
+                    >
+                        Contact Us
+                    </Typography>
+                </li>
+            </ul>
         </footer>
     );
 }
