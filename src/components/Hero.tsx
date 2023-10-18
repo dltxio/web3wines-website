c9eb33ef970e39ac6cb9765c21138b116d3106a6
maiha <maiha.219@gmail.com>
2023-10-18 13:00:45 +1000

    Change font for Web3 wines #16

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,10 @@
+import { Web3Wines } from "../images";
+
 export function Hero() {
   return (
     <div className="bg-cover bg-center h-screen flex items-center bg-cover items-center bg-hero-pattern bg-no-repeat">
-      <div className="container mx-auto text-center">
-        <h1 className="text-9xl font-bold text-white mb-4">Web3 Wines</h1>
+      <div className="container mx-auto grid justify-items-center">
+        <img src={Web3Wines} alt="Web3 Wines" />
       </div>
     </div>
   );
