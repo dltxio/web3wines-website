4b905a08d119bd3ffa0df2dadc1cb749599a2a66
maiha <maiha.219@gmail.com>
2023-10-20 11:18:08 +1000

    Rearrange Contact Us form #22

--- src/pages/ContactUs.tsx
+++ src/pages/ContactUs.tsx
@@ -34,6 +34,7 @@ const ContactUs: React.FC = () => {
               >
                 First name
               </label>
+              <p className="italic text-xs text-slate-400">(required)</p>
               <div className="mt-2.5">
                 <input
                   type="text"
@@ -41,6 +42,7 @@ const ContactUs: React.FC = () => {
                   id="first-name"
                   autoComplete="given-name"
                   className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  required
                 />
               </div>
             </div>
@@ -51,6 +53,7 @@ const ContactUs: React.FC = () => {
               >
                 Last name
               </label>
+              <p className="italic text-xs text-slate-400">(required)</p>
               <div className="mt-2.5">
                 <input
                   type="text"
@@ -58,6 +61,7 @@ const ContactUs: React.FC = () => {
                   id="last-name"
                   autoComplete="family-name"
                   className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  required
                 />
               </div>
             </div>
@@ -85,6 +89,7 @@ const ContactUs: React.FC = () => {
               >
                 Email
               </label>
+              <p className="italic text-xs text-slate-400">(required)</p>
               <div className="mt-2.5">
                 <input
                   type="email"
@@ -92,37 +97,7 @@ const ContactUs: React.FC = () => {
                   id="email"
                   autoComplete="email"
                   className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-navbar-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-            <div className="sm:col-span-2">
-              <label
-                htmlFor="phone-number"
-                className="block text-sm font-semibold leading-6 text-white"
-              >
-                Phone number
-              </label>
-              <div className="relative mt-2.5">
-                <div className="absolute inset-y-0 left-0 flex items-center">
-                  <label htmlFor="country" className="sr-only">
-                    Country
-                  </label>
-                  <select
-                    id="country"
-                    name="country"
-                    className="h-full rounded-md border-0 bg-transparent bg-none py-0 pl-4 pr-9 text-gray-900 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
-                  >
-                    <option>AU</option>
-                    <option>US</option>
-                    <option>EU</option>
-                  </select>
-                </div>
-                <input
-                  type="tel"
-                  name="phone-number"
-                  id="phone-number"
-                  autoComplete="tel"
-                  className="block w-full rounded-md border-0 px-3.5 py-2 pl-20 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  required
                 />
               </div>
             </div>
