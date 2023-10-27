3f42812829f6a4efe6e41caa248d6c3c8d541f57
maiha <maiha.219@gmail.com>
2023-10-27 10:47:14 +1000

    Pull FAQs from CMS

--- src/pages/FAQs.tsx
+++ src/pages/FAQs.tsx
@@ -1,27 +1,25 @@
-import React, { useLayoutEffect } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { Card, PageLayout } from "../components";
 
-const faqs = [
-  {
-    id: 1,
-    question: "What is NFT?",
-    answer:
-      "An NFT (non-fungible token) is a unique digital identifier that is recorded on a blockchain, and is used to certify ownership and authenticity. It cannot be copied, substituted, or subdivided. " +
-      "The ownership of an NFT is recorded in the blockchain and can be transferred by the owner, allowing NFTs to be sold and traded."
-  },
-  {
-    id: 2,
-    question: "What are some examples of using blockchain in wine industry?",
-    answer: "BitWine, Winechain, Cult Wines"
-  }
-];
 const FAQs: React.FC = () => {
-  useLayoutEffect(() => {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth"
-    });
+  const [faqs, setFaqs] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get("http://cms.dltx.io/api/faqs?populate=*")
+      .then(({ data }) => {
+        setFaqs(data.data);
+      })
+      .catch((error: any) => {
+        setError(error);
+      });
   }, []);
+
+  if (error) {
+    return <div> An error occurred: {error.message}</div>;
+  }
 
   return (
     <PageLayout>
@@ -40,18 +38,18 @@ const FAQs: React.FC = () => {
         </div>
       </div>
       <div className="bg-page py-12 sm:py-24 mx-auto max-w-7xl px-6 lg:px-8 text-center">
-        {faqs.map(item => (
+        {faqs.map(faq => (
           <Card>
             <div className="grid grid-cols-8 cols-2 p-8 sm:p-10 flex-auto text-justify text-navbar">
               <h1 className="col-span-2 text-8xl font-bold tracking-tight">
-                {item.id}
+                {faq.id}
               </h1>
               <div className="col-span-6">
                 <h3 className="text-xl font-bold tracking-tight ">
-                  {item.question}
+                  {faq.attributes.question}
                 </h3>
                 <p className="mt-2 leading-7 text-white group-hover:text-navbar">
-                  {item.answer}
+                  {faq.attributes.answer}
                 </p>
               </div>
             </div>
